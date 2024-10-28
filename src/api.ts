0d1260c8e5973dd2abf9c51a46d5a32085e0aec4
Ullala David <rkdrms2020@icloud.com>
2024-10-28 19:14:27 +0900

    React Query and Setting UI

---
+++ src/api.ts
@@ -0,0 +1,35 @@
+// api.ts
+import { QueryFunctionContext } from "@tanstack/react-query";
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "http://127.0.0.1:8000/api/v1/",
+});
+
+export const getRooms = () =>
+    instance.get("rooms/").then((response) => response.data);
+
+export const getRoom = async (roomPk: string | undefined) => {
+    if (!roomPk) throw new Error("Room PK is required");
+    return instance.get(`rooms/${roomPk}`).then((response) => response.data);
+};
+
+export const getRoomReviews = async (roomPk: string | undefined) => {
+    if (!roomPk) throw new Error("Room PK is required");
+    return instance.get(`rooms/${roomPk}/reviews`).then((response) => response.data);
+};
+
+// import { QueryFunctionContext } from "@tanstack/react-query";
+// import axios from "axios";
+
+// const instance = axios.create({
+//     baseURL: "http://127.0.0.1:8000/api/v1/",
+// });
+
+// export const getRooms = () =>
+//     instance.get("rooms/").then((response) => response.data);
+
+// export const getRoom = ({ queryKey }: QueryFunctionContext) => {
+//     const [_, roomPk] = queryKey;
+//     return instance.get(`rooms/${roomPk}`).then((response) => response.data);
+// };
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,36 +1,18 @@
-import { Box, Grid, HStack, Skeleton, SkeletonText } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Grid } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { getRooms } from "../api";
 import Room from "../components/Room";
 import RoomSkeleton from "../components/RoomSkeleton";
+import { IRoomList } from "../types";
 
-interface IPhoto {
-    pk: string;
-    file: string;
-    description: string;
-}
-interface IRoom {
-    pk: number;
-    name: string;
-    country: string;
-    city: string;
-    price: number;
-    rating: number;
-    is_owner: boolean;
-    photos: IPhoto[];
-}
 
 export default function Home() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [rooms, setRooms] = useState<IRoom[]>([]);
-    const fetchRooms = async () => {
-        const response = await fetch("http://127.0.0.1:8000/api/v1/rooms/");
-        const json = await response.json();
-        setRooms(json);
-        setIsLoading(false);
-    };
-    useEffect(() => {
-        fetchRooms();
-    }, []);
+
+    const { isLoading, data } = useQuery<IRoomList[]>({
+        queryKey: ["rooms"],
+        queryFn: getRooms,
+    });
+    
     return (
         <Grid
             mt={10}
@@ -62,8 +44,10 @@ export default function Home() {
             <RoomSkeleton />
         </>
     ) : null}
-    {rooms.map((room) => (
+    {data?.map((room) => (
         <Room
+            key={room.pk}
+            pk={room.pk}
             imageUrl={room.photos[0].file}
             name={room.name}
             rating={room.rating}
